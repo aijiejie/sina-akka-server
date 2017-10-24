10e616f8fce38c3f3aee8dd05a3c600e0382a4bb
aijiejie <125361152@qq.com>
2017-10-24 14:49:10 +0800

    导入akka服务器master分支初始文件
    master

---
+++ target/scala-2.11/classes/src/main/scala/models/DecisionTreeRegression.scala
@@ -0,0 +1,58 @@
+package models
+
+import java.io.File
+
+import org.apache.hadoop.fs.Path
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.{SparkConf, SparkContext}
+
+object DecisionTreeRegression {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  def  decisionTreeRegression(DTRMasterHost: String, DTRMasterPort: String, dtrTrainDataPath: String,predictDataPath:String,
+                              modelResultPath: String,predictResultPath:String, DTRName: String,
+                              impurity: String, maxDepth: Int, maxBins:Int) ={
+    //val conf = new SparkConf().setAppName("decisionTreeRegression-" + DTRName").setMaster("spark://master:7077")//集群模式
+    //val conf = new SparkConf().setAppName("decisionTreeRegression-" + DTRName").setMaster("yarn-client")//yarn模式
+    val conf = new SparkConf().setAppName("decisionTreeRegression-" + DTRName).setMaster("local")//本地模式
+    val sc = new SparkContext(conf)
+    val data = MLUtils.loadLibSVMFile(sc, dtrTrainDataPath)
+
+    // Split the data into training and test sets (30% held out for testing)
+    val splits = data.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
+
+    // Train a DecisionTree model.
+    //  Empty categoricalFeaturesInfo indicates all features are continuous.
+    val categoricalFeaturesInfo = Map[Int, Int]()
+
+
+    val model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo, impurity,
+      maxDepth, maxBins)
+
+    // Evaluate model on test instances and compute test error
+    val labelsAndPredictions = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testMSE = labelsAndPredictions.map{ case (v, p) => math.pow(v - p, 2) }.mean()//均方误差
+
+    //本地模式删除已存在模型
+    if (!modelResultPath.isEmpty) {
+      if (conf.get("spark.master") == "local") {
+        val file = new File(modelResultPath)
+        if (file.exists()) file.delete()
+      }
+      //集群模式删除已存在模型
+      val hadoopConf = sc.hadoopConfiguration
+      val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+      val path = new Path(modelResultPath)
+      hdfs.delete(path, true)
+      //保存模型
+      model.save(sc, modelResultPath)
+    }
+    sc.stop()
+    testMSE
+  }
+}
---
+++ target/scala-2.11/classes/src/main/scala/models/SVM.scala
@@ -0,0 +1,92 @@
+package models
+
+import java.io.File
+
+import org.apache.hadoop.fs.Path
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SVM {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  def svm(svmMasterHost:String,svmMasterPort:String,svmTrainDataPath:String,
+          svmPredictDataPath:String,svmModelResultPath:String,svmPredictResultPath:String,
+          name:String,iter:Int) = {
+    //val conf = new SparkConf().setAppName("SVM-" + name).setMaster("spark://master:7077")//集群模式
+    //val conf = new SparkConf().setAppName("SVM-" + name).setMaster("yarn-client")//yarn模式
+    val conf = new SparkConf().setAppName("SVM-" + name).setMaster("local")//本地模式
+    val sc = new SparkContext(conf)
+
+    // $example on$
+    // Load training data in LIBSVM format.
+    val data = MLUtils.loadLibSVMFile(sc,svmTrainDataPath)
+    //val stdData = standardization(data)//标准化标签为0，1的数据集
+
+
+    // Split data into training (60%) and test (40%).
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val training = splits(0).cache()
+    val test = splits(1)
+
+    // Run training algorithm to build the model
+    val numIterations = iter
+    val model = SVMWithSGD.train(training, numIterations)
+
+    // Clear the default threshold.
+    model.clearThreshold()
+
+    // Compute raw scores on the test set.
+    val scoreAndLabels = test.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
+    }
+
+    // Get evaluation metrics.
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
+    //metrics.roc().foreach(x=>println(x._1 + "," + x._2))
+
+    println("Area under ROC = " + auROC)
+
+    //本地模式删除已存在模型
+    if (!svmModelResultPath.isEmpty) {
+      if (conf.get("spark.master") == "local") {
+        val file = new File(svmModelResultPath)
+        if (file.exists()) file.delete()
+      }
+      //集群模式删除已存在模型
+      val hadoopConf = sc.hadoopConfiguration
+      val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+      val path = new Path(svmModelResultPath)
+      if (hdfs.exists(path)) {
+        hdfs.delete(path, true)
+      }
+      //保存模型
+      model.save(sc, svmModelResultPath)
+    }
+    //预测模型
+    if (!svmPredictDataPath.isEmpty){
+      val data = MLUtils.loadVectors(sc,svmTrainDataPath)
+      model.predict(data).saveAsTextFile(svmPredictResultPath)
+    }
+
+    sc.stop()
+    auROC
+  }
+
+
+  //将标签为1，2的标准化为0，1
+  def standardization(rdd:RDD[LabeledPoint]): RDD[LabeledPoint] ={
+    val newRdd = rdd.map(x=>{
+      val label = x.label-1
+      LabeledPoint(label,x.features)
+    })
+    newRdd
+  }
+}
